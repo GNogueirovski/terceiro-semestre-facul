b5b284bb18251aa4a8a438eaaac2dee4b0e136d8
gabriel <spbiel18@gmail.com>
2026-03-24 22:36:25 -0300

    spring class 1

---
+++ java-advanced/java/spring/spring-aula1/src/test/java/br/com/fiap/spring_aula1/SpringAula1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.spring_aula1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAula1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
